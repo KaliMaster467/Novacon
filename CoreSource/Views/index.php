69e8b0e8d35e90989c4d5ea4643f8e18e43f6aae
akx <reyesbrise@gmail.com>
2016-10-06 21:02:34 -0500

    background

--- CoreSource/Views/index.php
+++ CoreSource/Views/index.php
@@ -3,6 +3,19 @@
 	<title>Index</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="Public/Css/estiloIndex.css">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript">
+		$("#slide > div:gt(0)").hide();
+
+		setInterval(function() { 
+		  $('#slide > div:first')
+		    .fadeOut(1000)
+		    .next()
+		    .fadeIn(1000)
+		    .end()
+		    .appendTo('#slide');
+		},  3000);		
+	</script>
 </head>
 <body class="main-responsive-body">
 	<nav class="navbar">
@@ -14,6 +27,25 @@
 			<li class="navbar-art"><a href="GG">HomeSync</a></li>
 		</ul>
 	</nav>
+	<section class="main-publish">
+		<div id="slide">
+			<div>
+				<div class="bdone">
+					
+				</div>
+			</div>
+			<div>
+				<div class="bdtwo">
+					
+				</div>
+			</div>
+			<div>
+				<div class="bdthree">
+					
+				</div>
+			</div>
+		</div>
+	</section>
 
 </body>
 </html>
